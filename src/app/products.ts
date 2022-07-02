d164e77f383accdadeeacc62770e86b851128b2f
dcore099 <dcore099@gmail.com>
2022-07-02 17:46:07 -0500

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  discount: number;
 }
 
 export const products = [
@@ -10,7 +11,8 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    discount: 0;
   },
   {
     id: 2,
