7ea0de6c9045782952f15e0c47aac170474985b9
dcore099 <dcore099@gmail.com>
2022-07-02 17:52:32 -0500

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -12,19 +12,21 @@ export const products = [
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens',
-    discount: 0;
+    discount: 5
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    discount: 15
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'Some shitty phone',
+    discount: 0
   }
 ];
 
